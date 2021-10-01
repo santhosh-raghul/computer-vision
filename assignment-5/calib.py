4a172a3ace040c0b1761b131f3ad953056e1dd78
Santhosh Raghul <gssanthoshraghul1010@gmail.com>
2021-10-01 13:21:23 +0530

    updated calib.py | added test images

--- assignment-5/calib.py
+++ assignment-5/calib.py
@@ -1,12 +1,15 @@
-#!/usr/bin/env python
+#! /usr/bin/env python3
 
 import cv2
 import numpy as np
 import os
 
 CHECKERBOARD = (5,7)
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-print(criteria)
+PATH = './images/set-2'
+
+# criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# print(criteria)
+
 objpoints = []
 imgpoints = [] 
 
@@ -14,30 +17,28 @@ objp = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
 objp[0,:,:2] = np.lib.index_tricks.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
 prev_img_shape = None
 
-images = os.listdir('.')
-images = [i for i in images if i.endswith('.jpg')]
+images = os.listdir(PATH)
+images = [i for i in images if i.startswith("chessboard_") and i.endswith('.jpg')]
 
 for img_path in images:
-    img = cv2.imread(img_path, 0)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-    ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
-    
-    if ret == True:
-        objpoints.append(objp)
-        corners2 = cv2.cornerSubPix(gray, corners, (11,11),(-1,-1), criteria)
-        
-        imgpoints.append(corners2)
-
-        img = cv2.drawChessboardCorners(img, CHECKERBOARD, corners2, ret)
-    
-    cv2.imshow('img',img)
-    cv2.waitKey(0)
+	print(f"{PATH}/{img_path}")
+	img = cv2.imread(f"{PATH}/{img_path}")
+	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
+	
+	if ret == True:
+		objpoints.append(objp)
+		# corners2 = cv2.cornerSubPix(gray, corners, (11,11),(-1,-1), criteria)
+		imgpoints.append(corners)
+		img = cv2.drawChessboardCorners(img, CHECKERBOARD, corners, ret)
+	
+	cv2.imshow('img',img)
+	cv2.waitKey(0)
 
 cv2.destroyAllWindows()
 
 h,w = img.shape[:2]
-
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
 print("Camera matrix : \n")
@@ -48,3 +49,18 @@ print("rvecs : \n")
 print(rvecs)
 print("tvecs : \n")
 print(tvecs)
+
+images = os.listdir(PATH)
+
+for img_path in images:
+
+	img = cv2.imread(f"{PATH}/{img_path}")
+	h, w = img.shape[:2]
+	newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+	undist = cv2.undistort(img, mtx, dist, None, newcameramtx)
+	x, y, w, h = roi
+	undist_cropped = undist[y:y+h, x:x+w]
+	cv2.imshow('original', img)
+	# cv2.imshow('undistorted full', undist)
+	cv2.imshow('undistorted cropped', undist_cropped)
+	cv2.waitKey(0) 
